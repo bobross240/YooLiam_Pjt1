92654d237effb245c806fd042d1a5b8d279eeec4
liam <lsyoo@wpi.edu>
2018-01-31 14:44:36 -0500

    init2

--- CS4731_Proj1.js
+++ CS4731_Proj1.js
@@ -4,6 +4,8 @@ function main()
     var canvas = document.getElementById('webgl');
     var gl = WebGLUtils.setupWebGL(canvas);
 
+    var selectedFile = document.getElementById('input').files[0];
+
     //Get rendering context
     if(!gl)
     {
@@ -45,35 +47,5 @@ function main()
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.POINTS, 0, points.length);
-
-
-    // window.onclick = function(event) {
-    //     var i = index;
-    //     x=2*event.clientX/canvas.width-1;
-    //     y=2*(canvas.height-event.clientY)/canvas.height-1;
-    //     var pts = [x, y];
-    //     console.log(x, y);
-    //     points.push(pts);
-    //
-    //     gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-    //     gl.bufferSubData(gl.ARRAY_BUFFER, i, new Float32Array(pts));
-    //     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    //     gl.bufferSubData(gl.ARRAY_BUFFER, i, new Float32Array(colors));
-    //     index++;
-    // }
-    //
-    // window.onkeypress = function(event)
-    // {
-    //     var key = String.fromCharCode(event.keyCode);
-    //     switch(key)
-    //     {
-    //         case 'a':
-    //             canvas.width = 200;
-    //             gl.clear(gl.COLOR_BUFFER_BIT);
-    //             gl.drawArrays(gl.TRIANGLES, 0, points.length);
-    //             window.alert('Key pressed is ' + key);
-    //             break;
-    //     }
-    // }
 
 }
